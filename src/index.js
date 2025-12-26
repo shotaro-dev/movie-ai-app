58f449c43443b126aa8f1cecd99f2a4bc4e69cf5
shotaro-dev <231440159+shotaro-dev@users.noreply.github.com>
2025-12-26 19:39:41 +0900

    Feat: Add a function to save the contents of content.js with embeddings to Supabase.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import { openai, supabase } from "./config.js";
 
 const form = document.getElementById("movie-form");
 const resultEl = document.getElementById("recommended-movie-result");
+const recommendedMovieSection = document.getElementById(
+  "recommended-movie-section"
+);
+const goAgainButton = document.getElementById("go-again-button");
+const movieFormSection = document.getElementById("movie-form-section");
 
 // taxtareaからのinputsを受け取り、joinする
 
@@ -14,21 +19,57 @@ form.addEventListener("submit", async (e) => {
   const inputs = formData.getAll("question");
   const query = inputs.join("\n");
 
-  const embedding = await embeddingQuery(query);
+  const embedding = await createEmbedding(query);
   console.log(embedding);
 });
 
 // console.log("test");
 
-async function embeddingQuery(query) {
-  const response = await openai.embeddings.create({
-    model: "text-embedding-3-small",
-    input: query,
-  });
-  const embedding = response.data[0].embedding;
-  return embedding;
-  // console.log(response.data[0].embedding);
+async function createEmbedding(input) {
+  try {
+    const response = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: input,
+    });
+    const embedding = response.data[0].embedding;
+    return embedding;
+  } catch (error) {
+    console.error("Embedding API error:", error);
+    throw error;
+  }
 }
 
+// content.jsをベクトル化して、supabaseに保存するコードを作成する
+async function storeMovieEmbeddings() {
+  const movies = await import("./content.js");
+  try {
+    const data = await Promise.all(
+      movies.default.map(async (movie) => {
+        const { title, releaseYear, content } = movie;
+        const embeddingText = `${title}\n(${releaseYear})\n${content}`;
+        const embedding = await createEmbedding(embeddingText);
+        return {
+          title,
+          release_year: releaseYear,
+          content,
+          embedding,
+        };
+      })
+    );
+    if (data.length === 0) {
+      throw new Error("No movie data to store.");
+    }
 
+    const { error } = await supabase.from("movies").insert(data);
+    if (error) {
+      throw new Error("Supabase insert error: " + error.message);
+    }
 
+    console.log(
+      "All movie embeddings stored. " + data.length + " records added."
+    );
+  } catch (error) {
+    console.error("Error storing movie embeddings: ", error);
+  }
+}
+// storeMovieEmbeddings();
