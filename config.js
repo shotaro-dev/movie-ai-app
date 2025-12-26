75c8b64ebe571db90d62647070a7a5a6e1eb36bd
shotaro-dev <231440159+shotaro-dev@users.noreply.github.com>
2025-12-26 09:36:54 +0900

    Fix: Correct the incorrect way of calling the apiKey in vite. Also make additional fixes to the HTML.

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 import { createClient } from "@supabase/supabase-js";
 
 /** OpenAI config */
-if (!process.meta.process.env.VITE_OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!import.meta.env.VITE_OPENAI_API_KEY)
+  throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-  apiKey: meta.process.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 /** Supabase config */
-const privateKey = process.meta.process.env.VITE_SUPABASE_API_KEY;
+const privateKey = import.meta.env.VITE_SUPABASE_API_KEY;
 if (!privateKey) throw new Error(`Expected env var VITE_SUPABASE_API_KEY`);
-const url = process.meta.process.env.VITE_SUPABASE_URL;
+const url = import.meta.env.VITE_SUPABASE_URL;
 if (!url) throw new Error(`Expected env var VITE_SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
