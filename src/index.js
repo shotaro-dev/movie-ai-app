66981693b9cbcc6f78bc54e1dbd355c08b357739
shotaro-dev <231440159+shotaro-dev@users.noreply.github.com>
2025-12-27 09:43:52 +0900

    feat: Add movie search functionality from the database. Adjust HTML form names for clarity.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const recommendedMovieSection = document.getElementById(
 const goAgainButton = document.getElementById("go-again-button");
 const movieFormSection = document.getElementById("movie-form-section");
 
+const QUESTIONS = [
+  "What is your favorite movie and why?",
+  "Are you in the mood for something new or a classic?",
+  "Do you wanna have fun or do you want somthing serious?",
+];
+
 // taxtareaからのinputsを受け取り、joinする
 
 form.addEventListener("submit", async (e) => {
@@ -16,25 +22,49 @@ form.addEventListener("submit", async (e) => {
   //   textareaからの入力を取得
   const formData = new FormData(form);
   // formdataからの値を取得
-  const inputs = formData.getAll("question");
-  const query = inputs.join("\n");
+  const inputs = formData.getAll("answer");
+  const query = QUESTIONS.map((question, index) => {
+    return `${question}\n${inputs[index]}`;
+  }).join("\n");
+  console.log("User query:", query);
 
   const embedding = await createEmbedding(query);
-  console.log(embedding);
+  // console.log(embedding);
+  const similarMovies = await searchSimilarMovies(embedding);
+  console.log(similarMovies);
 });
 
 // console.log("test");
 
-async function createEmbedding(input) {
+async function createEmbedding(text) {
   try {
     const response = await openai.embeddings.create({
       model: "text-embedding-3-small",
-      input: input,
+      input: text,
     });
     const embedding = response.data[0].embedding;
     return embedding;
   } catch (error) {
     console.error("Embedding API error:", error);
+    throw error;
+  }
+}
+
+// ベクトルの類似性で映画を検索する
+async function searchSimilarMovies(embedding) {
+  try {
+    const { data, error } = await supabase.rpc("search_similar_movies", {
+      query_embedding: embedding,
+      similarity_match_threshold: 0.2,
+      match_count: 1,
+    });
+    if (error) {
+      throw error;
+    }
+    console.log("Supabase search data:", data);
+    return data;
+  } catch (error) {
+    console.error("Supabase search error:", error);
     throw error;
   }
 }
